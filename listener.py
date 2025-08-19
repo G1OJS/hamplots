04219a4f4c3c00a0171a887b90832f33ac940788
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-19 22:51:15 +0100

    Update listener.py

--- listener.py
+++ listener.py
@@ -4,7 +4,7 @@ import ast
 import datetime
 import time
 
-global myBands, myModes, mySquares
+global myBands, myModes, mySquares, decodes
 
 def get_cfg():
     global myBands, myModes, mySquares
@@ -17,37 +17,24 @@ def get_cfg():
     print(f"myBands {myBands}")
     print(f"myModes {myModes}")
 
-def sub_listen_dump(self, client, userdata, flags, reason_code, properties):
+def subscribe(client, userdata, flags, reason_code, properties):
+    global decodes
     # pskr/filter/v2/{band}/{mode}/{sendercall}/{receivercall}/{senderlocator}/{receiverlocator}/{sendercountry}/{receivercountry}
     print(f"Connected: {reason_code}")
-    for sq in self.squares:
-        for b in self.bands:
-            for md in self.modes:
+    for sq in mySquares:
+        for b in myBands:
+            for md in myModes:
                 for TxRx in ["Rx","Tx"]:
                     print(f"Subscribe to {TxRx} in {sq} on {b} {md}")
                     tailstr = f"+/+/{sq}/+/+/#" if TxRx == "Tx" else f"+/+/+/{sq}/+/#"
                     client.subscribe(f"pskr/filter/v2/{b}/{md}/{tailstr}")
-                  
-    mqtt_cl.loop_start()
-    sleep(time_seconds)
-    mqtt_cl.loop_stop()
-    mqtt_cl.disconnect()
 
-    frx = open("Rx_decodes.csv","a")
-    ftx = open("Tx_decodes.csv","a")
-    for d in decodes:
-        ebfm = f"{d['t']}, {d['b']}, {d['f']}, {d['md']}, "
-        spot = f"{d['hc']}, {d['hl']}, {d['ha']}, {d['TxRx']}, {d['oc']}, {d['ol']}, {d['oa']}, {d['rp']}\n"
-        f = ftx if d['TxRx'] == "Tx" else frx
-        f.write(ebfm+spot)
-        f.flush()
-    decodes = []
-
-def add_decode(self, client, userdata, msg):
+def add_decode(client, userdata, msg):
+    global decodes
     d = ast.literal_eval(msg.payload.decode())
     d['sl'] = d['sl'].upper()
     d['rl'] = d['rl'].upper()
-    if(d['sl'] in mySquares) TxRx = "Tx" else "Rx"
+    TxRx = "Tx" if(d['sl'] in mySquares) else "Rx"
     d.update({'TxRx':TxRx})
     d.update({'hc':  d['rc'] if TxRx =="Rx" else d['sc']})
     d.update({'hl':  d['rl'] if TxRx =="Rx" else d['sl']})
@@ -57,10 +44,28 @@ def add_decode(self, client, userdata, msg):
     d.update({'oa':  d['sa'] if TxRx =="Rx" else d['ra']})
     decodes.append(d)
 
+decodes = []
 get_cfg()
-mqtt_cl = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id = f"subscribe-{self.TxRx}")
-mqtt_cl.on_connect = sub_listen_dump
+mqtt_cl = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_cl.on_connect = subscribe
 mqtt_cl.on_message = add_decode
 mqtt_cl.connect("mqtt.pskreporter.info", 1883, 60)
+
+time_seconds = 30
+mqtt_cl.loop_start()
+time.sleep(time_seconds)
+mqtt_cl.loop_stop()
+mqtt_cl.disconnect()
+
+frx = open("Rx_decodes.csv","a")
+ftx = open("Tx_decodes.csv","a")
+for d in decodes:
+    ebfm = f"{d['t']}, {d['b']}, {d['f']}, {d['md']}, "
+    spot = f"{d['hc']}, {d['hl']}, {d['ha']}, {d['TxRx']}, {d['oc']}, {d['ol']}, {d['oa']}, {d['rp']}\n"
+    f = ftx if d['TxRx'] == "Tx" else frx
+    f.write(ebfm+spot)
+f.flush()
+
+
 
 
